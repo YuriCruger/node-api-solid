f7391a1af09561d47d516b33c82d2dadf52f3a37
Yuri Cruger <aoddrewiid@gmail.com>
2024-04-30 13:07:44 -0300

    implementing use cases

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,7 +1,11 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 export class PrismaUsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
